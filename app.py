b8926ce0f16fff3452be41c240479b341fb537b0
Simon Z. Ghebremeskel <81813787+simon-zerisenay@users.noreply.github.com>
2024-03-01 13:03:56 +0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 # file_path = '/Users/simon/Desktop/Builds/Dashboard/race_20240103_042233.csv' 
 file_path = '/Users/simon/Desktop/Builds/Dashboard/final.csv' 
 
-data = pd.read_csv(file_path)
+data = pd.read_csv(file_path) 
  
 # Display the first few rows of the dataframe to understand its structure
 print(data.head())
