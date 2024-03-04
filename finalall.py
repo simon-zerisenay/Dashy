48d6758ecb088bab92d82b558715af2455441b9f
Simon Z. Ghebremeskel <81813787+simon-zerisenay@users.noreply.github.com>
2024-03-04 15:45:19 +0400

    Update finalall.py

--- finalall.py
+++ finalall.py
@@ -10,7 +10,7 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 # Navigation bar
 navbar = dbc.NavbarSimple(
     children=[
-        dbc.NavItem(dbc.NavLink("Dashboard", href="/")),
+        dbc.NavItem(dbc.NavLink("Dashboard", href="/")), 
         dbc.NavItem(dbc.NavLink("About Us", href="/about")),  
         dbc.NavItem(dbc.NavLink("Contact", href="/contact")),
     ],
