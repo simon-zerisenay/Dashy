f09bb5ef7e98b748b0d830b1137058b60eaa53a9
Simon Z. Ghebremeskel <81813787+simon-zerisenay@users.noreply.github.com>
2024-03-05 07:59:52 +0400

    Update data.py

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ import plotly.express as px
 # Load the CSV file
 # file_path = '/path/to/your/csv/file.csv'  # Make sure to update this path to your actual file location
 file_path = '/Users/simon/Desktop/Builds/Dashboard/race_20240103_042233.csv'
-data = pd.read_csv(file_path) 
+data = pd.read_csv(file_path)  
 
 # Assuming the first column is 'Time' and it's in a recognizable format, convert it to datetime
 data['Time'] = pd.to_datetime(data.iloc[:, 0], format='%H:%M:%S.%f', errors='ignore')
