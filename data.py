53f3a8d25cd7bb820a9ce70dd87645da14bd91c0
Simon Z. Ghebremeskel <81813787+simon-zerisenay@users.noreply.github.com>
2024-03-05 07:59:47 +0400

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import plotly.express as px
+import plotly.express as px 
  
 # Load the CSV file
 # file_path = '/path/to/your/csv/file.csv'  # Make sure to update this path to your actual file location
